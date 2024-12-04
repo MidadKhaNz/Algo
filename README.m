02ecfc536272ae205e7ffe3c94bfdb499b87aee3
Midad KhaNz <87993578+MidadKhaNz@users.noreply.github.com>
2024-12-04 08:17:02 +0600

    LCS

---
+++ README.m
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string X, Y;
+    cin >> X >> Y;
+
+    int m = X.size(), n = Y.size();
+    int dp[m + 1][n + 1];
+
+    for (int i = 0; i <= m; i++) {
+        for (int j = 0; j <= n; j++) {
+            dp[i][j] = 0;
+        }
+    }
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (X[i - 1] == Y[j - 1])
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    string lcs = "";
+    int i = m, j = n;
+    while (i > 0 && j > 0) {
+        if (X[i - 1] == Y[j - 1]) {
+            lcs = X[i - 1] + lcs;
+            i--;
+            j--;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+
+    cout << "Longest Common Subsequence: " << lcs << endl;
+    return 0;
+}
